78acde62cbe39e0727bf1e0c19d0c1c0a75ede0a
Thomas Bassetto <tbassetto@gmail.com>
2023-08-18 17:11:49 +0200

    First commit, based on Astro template

---
+++ src/consts.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Thomas Bassetto';
+export const SITE_DESCRIPTION = 'My personal website';
+export const MASTODON = 'https://oslo.town/@tbassetto';
+export const X = 'https://twitter.com/tbassetto';
+export const GITHUB = 'https://github.com/tbassetto';
