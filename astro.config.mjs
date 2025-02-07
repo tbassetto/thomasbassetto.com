783941835b8f79f56ad151f4eba25fffdbbd6530
Thomas Bassetto <tbassetto@gmail.com>
2025-02-07 21:39:30 +0100

    Update Astro and Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	site: 'https://thomasbassetto.com',
-	integrations: [mdx(), sitemap(), tailwind()],
+	integrations: [mdx(), sitemap()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
