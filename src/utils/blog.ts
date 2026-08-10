3641930371ab78f964bbb599d5157a7a80605c62
Thomas Bassetto <tbassetto@gmail.com>
2026-08-10 14:03:33 +0200

    Update code based on Codex recommendations

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -1,9 +1,7 @@
 import type { CollectionEntry } from 'astro:content';
 
-const BLOG_ENTRY_EXTENSION = /\.(md|mdx)$/;
-
 export function getBlogEntrySlug(entry: CollectionEntry<'blog'>) {
-	return entry.slug ?? entry.id.replace(BLOG_ENTRY_EXTENSION, '');
+	return entry.id;
 }
 
 export function getBlogEntryHref(entry: CollectionEntry<'blog'>) {
